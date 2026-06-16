48378b97bccb08367c4cabbca34be8a531c3e21b
Gyorge <gyorge.moreira@escola.pr.gov.br>
2026-06-16 11:50:44 +0000

    Python

---
+++ backend.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# servir arquivos estáticos
+app.mount("/front-end", StaticFiles(directory="front-end"), name="front-end")
+
+
+# rota que o JS vai chamar
+@app.get("/api/somar")
+def somar(a: int, b: int):
+    return {"resultado": a * b}
+
+@app.get("/api/somar")
+def somar(a: int, b: int):
+    return {"resultado": a * b}
+
+
+# abrir o site
+@app.get("/")
+def home():
+    return FileResponse("front-end/index.html")
